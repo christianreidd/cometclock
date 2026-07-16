5e40cdcd26ea09484550a59ef8d8969e640d6bd7
Christian Reid <z5768981@unsw.edu.au>
2026-07-16 21:35:02 +1000

    refactored convertTime function to cover edge cases

--- script.js
+++ script.js
@@ -99,18 +99,15 @@ function convertTime(taskInfo) {
     const min = timeArray[1];
     let timeSuffix = "am";
 
-    if ((hour - 12) >= 0) {
-        if (hour != 12) {
-            hour = hour - 12;
-        }
+    if (hour === 0) {
+        hour = 12;
+    } else if (hour === 12) {
+        timeSuffix = "pm";
+    } else if (hour > 12) {
+        hour -= 12;
         timeSuffix = "pm";
     }
-
-    if (hour === "0") {
-        hour = 12;
-        timeSuffix = "am";
-    }
-
+    
     return `${hour}:${min} ${timeSuffix}`
 }
 
